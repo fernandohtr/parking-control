50d0c5e11da3fc0cccf836ce8e8341423b60205e
Fernando Toledo <fernandohtr@gmail.com>
2022-11-01 17:17:12 -0300

    feature: add simple route to test
    - fix typo

--- src/test/java/com/api/parkingcontrol/ParkingControlApplicationTests.java
+++ src/test/java/com/api/parkingcontrol/ParkingControlApplicationTests.java
@@ -1,10 +1,10 @@
-package com.api.parkgincontrol;
+package com.api.parkingcontrol;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ParkginControlApplicationTests {
+class ParkingControlApplicationTests {
 
 	@Test
 	void contextLoads() {
