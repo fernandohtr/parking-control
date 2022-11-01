19bfe73934308e0dd53ee5ea7802bb6161946128
Fernando Toledo <fernandohtr@gmail.com>
2022-10-31 21:39:39 -0300

    first commit

---
+++ src/main/java/com/api/parkgincontrol/ParkginControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.parkgincontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkginControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkginControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/parkgincontrol/ParkginControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.parkgincontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkginControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
